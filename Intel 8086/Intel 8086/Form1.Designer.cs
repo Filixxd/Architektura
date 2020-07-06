30f1be24e5519ee39606301fca16b176818f7152
Filixxd <61656152+Filixxd@users.noreply.github.com>
2020-07-06 23:47:27 +0200

    Add files via upload

---
+++ Intel 8086/Intel 8086/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Intel_8086
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(259, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INTEL 8086";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(204, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CX";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(203, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "DX";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(391, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(391, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(391, 192);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(391, 221);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.listBox1.Location = new System.Drawing.Point(292, 262);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(27, 56);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(266, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "z";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(336, 282);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "do";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.listBox2.Location = new System.Drawing.Point(361, 262);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(29, 56);
+            this.listBox2.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 26);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Przenieś";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Items.AddRange(new object[] {
+            "MOVE"});
+            this.listBox3.Location = new System.Drawing.Point(215, 282);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(45, 17);
+            this.listBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Intel 8086/Intel 8086/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Intel_8086
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string curItem = listBox1.SelectedItem.ToString();
+            string curItem2 = listBox2.SelectedItem.ToString();
+            string function = listBox3.SelectedItem.ToString();
+            if (function == "MOVE" && curItem == "AX" && curItem2 == "AX")
+            {
+                textBox1.Text = textBox1.Text;
+            }
+            else if(function == "MOVE" && curItem == "AX" && curItem2 == "BX")
+            {
+                textBox2.Text=textBox1.Text;
+            }
+            else if (function == "MOVE" && curItem == "AX" && curItem2 == "CX")
+            {
+                textBox3.Text = textBox1.Text;
+            }
+            else if (function == "MOVE" && curItem == "AX" && curItem2 == "DX")
+            {
+                textBox4.Text = textBox1.Text;
+            }
+            else if (function == "MOVE" && curItem == "BX" && curItem2 == "AX")
+            {
+                textBox1.Text = textBox2.Text;
+            }
+            else if (function == "MOVE" && curItem == "BX" && curItem2 == "BX")
+            {
+                textBox2.Text = textBox2.Text;
+            }
+            else if (function == "MOVE" && curItem == "BX" && curItem2 == "CX")
+            {
+                textBox3.Text = textBox2.Text;
+            }
+            else if (function == "MOVE" && curItem == "BX" && curItem2 == "DX")
+            {
+                textBox4.Text = textBox2.Text;
+            }
+            else if (function == "MOVE" && curItem == "BX" && curItem2 == "BX")
+            {
+                textBox2.Text = textBox2.Text;
+            }
+            else if (function == "MOVE" && curItem == "CX" && curItem2 == "AX")
+            {
+                textBox1.Text = textBox3.Text;
+            }
+            else if (function == "MOVE" && curItem == "CX" && curItem2 == "BX")
+            {
+                textBox2.Text = textBox3.Text;
+            }
+            else if (function == "MOVE" && curItem == "CX" && curItem2 == "CX")
+            {
+                textBox3.Text = textBox3.Text;
+            }
+            else if (function == "MOVE" && curItem == "CX" && curItem2 == "DX")
+            {
+                textBox4.Text = textBox3.Text;
+            }
+            else if (function == "MOVE" && curItem == "DX" && curItem2 == "AX")
+            {
+                textBox1.Text = textBox4.Text;
+            }
+            else if (function == "MOVE" && curItem == "DX" && curItem2 == "BX")
+            {
+                textBox2.Text = textBox4.Text;
+            }
+            else if (function == "MOVE" && curItem == "DX" && curItem2 == "CX")
+            {
+                textBox3.Text = textBox4.Text;
+            }
+            else if (function == "MOVE" && curItem == "DX" && curItem2 == "DX")
+            {
+                textBox4.Text = textBox4.Text;
+            }
+        }
+    }
+}
